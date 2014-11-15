60daa7cdcc1cfc044720eb28f7ad5ef121b9368c
Miklós <Mikls@Miki-PC>
2014-11-15 12:39:16 +0100

    Project init commit

---
+++ src/laystream/LayNode.java
@@ -0,0 +1,90 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package laystream;
+
+import java.awt.Point;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Miklós
+ */
+public class LayNode {
+
+    private String id;
+    private Point location;
+    private ArrayList<String> pss;
+    private ArrayList<String> tc;
+    private ArrayList<String> sentPackets;
+    private ArrayList<String> receivedPackets;
+
+    public LayNode(String id, Point location) {
+        this.id = id;
+        this.location = location;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Point getLocation() {
+        return location;
+    }
+
+    public void setLocation(Point location) {
+        this.location = location;
+    }
+
+    public ArrayList<String> getPss() {
+        return pss;
+    }
+
+    public void setPss(ArrayList<String> pss) {
+        this.pss = pss;
+    }
+
+    public ArrayList<String> getTc() {
+        return tc;
+    }
+
+    public void setTc(ArrayList<String> tc) {
+        this.tc = tc;
+    }
+
+    public String toString() {
+        return "V" + id;
+    }
+
+    public ArrayList<String> getSentPackets() {
+        return sentPackets;
+    }
+
+    public void setSentPackets(ArrayList<String> sentPackets) {
+        this.sentPackets = sentPackets;
+    }
+
+    public ArrayList<String> getReceivedPackets() {
+        return receivedPackets;
+    }
+
+    public void setReceivedPackets(ArrayList<String> receivedPackets) {
+        this.receivedPackets = receivedPackets;
+    }
+
+    private double getDistanceFromOther(LayNode node) {
+        double xDistance = Math.abs(this.getLocation().x - node.getLocation().x);
+        double yDistance = Math.abs(this.getLocation().y - node.getLocation().y);
+        double distance = Math.sqrt((xDistance * xDistance) + (yDistance * yDistance));
+
+        return distance;
+
+    }
+
+}
---
+++ src/laystream/Util.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package laystream;
+
+/**
+ *
+ * @author Miklós
+ */
+public class Util {
+
+    public static final int C = 8;
+    public static final int K = 8;
+
+}
